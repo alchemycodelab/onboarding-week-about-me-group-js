693e9765dfec664041f7149fb5a7e794d334be6e
sarah-svg <kjskillen@gmail.com>
2022-01-11 14:53:25 -0800

    working will update the bio for each person

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
 // import functions
+import { people } from './data.js';
+// console.log(people);
 
 // grab DOM elements
 
+
+const selectEl = document.querySelector('#name');
+// console.log(template, container, selectEl);
+
+const nameEl = document.querySelector('h1');
+const bioEl = document.querySelector('p');
+const hobbiesEl = document.querySelector('ul');
+const ageEl = document.querySelector('span');
+
+
 // set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+
+selectEl.addEventListener('change', (e) => { 
+        // use user input to update state
+    hobbiesEl.innerHTML = '';
+
+    const selected = e.target.value;
+    
+    if (selected === 'one') {
+     
+        renderPerson(0);
+    } if (selected === 'two') { 
+ 
+        renderPerson(1);
+    } if (selected === 'three') { 
+
+        renderPerson(2);
+    } if (selected === 'four') { 
+
+        renderPerson(3);
+    }
+    
+});
+
+function renderPerson(index) {
+     // update DOM to reflect the new state
+    // clearAll();
+    return nameEl.textContent = people[index].name,
+    bioEl.textContent = people[index].bio,
+    ageEl.textContent = people[index].age,
+    people[index].hobbies.map((hobby) => {
+        const li = document.createElement('li');
+        li.textContent = hobby;
+        hobbiesEl.appendChild(li);
+    });
+}
---
+++ data.js
@@ -0,0 +1,30 @@
+export const people = [
+    { 
+        id: 1,
+        name: 'John',
+        age: 20,
+        bio: 'John  is a senior software engineer at Google.',
+        hobbies: ['hiking', 'cooking', 'coding']
+    },
+    {
+        id: 2,
+        name: 'Jane',
+        age: 30,
+        bio: 'Jane is a senior software engineer at Apple.',
+        hobbies: ['knitting', 'sewing', 'coding']
+    },
+    {
+        id: 3,
+        name: 'Bob',
+        age: 40,
+        bio: 'Bob is a senior software engineer at Microsoft.',
+        hobbies: ['basketball', 'gaming', 'coding']
+    },
+    {
+        id: 4,
+        name: 'Mary',
+        age: 50,
+        bio: 'Mary is a senior software engineer at Amazon.',
+        hobbies: ['painting', 'cooking', 'coding']
+    }
+];
